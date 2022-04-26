c8783f04fd17cc2f52c0c65776d97b24972e20b9
James <james@touthang.info>
2022-04-26 13:41:50 -0500

    test

--- compress_activity.sh
+++ compress_activity.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npx --yes --package inline-source-cli inline-source --attribute "" --compress true --root ./build/web ./build/web/index.html > docs/index.html
+#npx --yes --package inline-source-cli inline-source --attribute "" --compress true --root ./build/web ./build/web/index.html > docs/index.html
 openssl base64 -in docs/index.html -out docs/index.html.b64
